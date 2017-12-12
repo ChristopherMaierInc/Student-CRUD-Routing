cb098a081f34ea39a20234684b66780457d1c377
mail@christophermaier.com.au <mail@christophermaier.com.au>
2017-12-12 14:14:51 +1000

    Added anchor tag around profile picture to link to individual profile picture

--- src/components/ListItem.js
+++ src/components/ListItem.js
@@ -3,7 +3,7 @@ import React from 'react';
 const ListItem = ({ name, photo, bio, id }) => (
   <div>
     <ul>
-      <li><img className="avatar" src={ photo } alt={ bio }/></li>
+      <li><a href={`/students/${id}`}><img className="avatar" src={ photo } alt={ bio }/></a></li>
       <li><strong>{ name }</strong></li>
       <li>{ bio }</li>
       <li><button>Delete</button></li>
