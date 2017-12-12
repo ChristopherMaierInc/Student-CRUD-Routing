16b6880180372c9b76bb8213574cf432e7cffe32
mail@christophermaier.com.au <mail@christophermaier.com.au>
2017-12-12 12:19:02 +1000

    Listed all students to home page and seperated our concerns

--- src/App.js
+++ src/App.js
@@ -1,21 +1,27 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import StudentList from './components/StudentList'
 
 class App extends Component {
+
+state = {
+  students: [
+    { id: 1, name: "Jim James", photo: "pic.jpg", bio: "Coder with no home" },
+    { id: 2, name: "Jim Jomes", photo: "pic.jpg", bio: "Coder with two homes" },
+    { id: 3, name: "Jim Jumes", photo: "pic.jpg", bio: "Coder with cardboard box" },
+    { id: 4, name: "Jim Jemes", photo: "pic.jpg", bio: "Coder with a pet snail" }
+  ]
+}
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <StudentList students={ this.state.students } />
       </div>
     );
   }
 }
+
+
 
 export default App;
---
+++ src/components/StudentList.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+const StudentList = ({ students }) => (
+  <div>
+    {
+      students.map(student => { return <p>{ student.name }</p> })
+    }
+  </div>
+)
+
+export default StudentList;
